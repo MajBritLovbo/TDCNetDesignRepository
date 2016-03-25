8839d926efb75d16d4205e4fd935526cdaf58d25
Maj-Brit Bech Lovbo <mb.lovbo@gmail.com>
2016-03-25 10:19:28 +0100

    Initial commit

---
+++ ConsoleWatch/src/main/java/programmingtest/TDCNetDesign/ConsoleWatch/App.java
@@ -0,0 +1,13 @@
+package programmingtest.TDCNetDesign.ConsoleWatch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
