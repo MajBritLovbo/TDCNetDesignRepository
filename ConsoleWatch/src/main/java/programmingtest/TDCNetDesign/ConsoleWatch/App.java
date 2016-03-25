826fc14a105962521425de6e4410e55c263c09f0
Maj-Brit Bech Lovbo <mb.lovbo@gmail.com>
2016-03-25 12:42:58 +0100

    updated main with method-calls. updated the initializing of the frequency.

--- ConsoleWatch/src/main/java/programmingtest/TDCNetDesign/ConsoleWatch/App.java
+++ ConsoleWatch/src/main/java/programmingtest/TDCNetDesign/ConsoleWatch/App.java
@@ -18,25 +18,51 @@ public class App
 	private static int frequency=1; // 1/1000s
 	private static BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
 	private static Clock cl = Clock.systemUTC();
-	
+
 	public static void main( String[] args ) 
 	{ 
+
+		initFrequency();
+		run();
+	}
+
+	private static void setFrequency(int newTime) {
+		frequency=newTime*1000;		
+	} 
+
+	private static void setClock(Clock clock){
+		cl=clock;
+	}
+
+	private static void initFrequency(){
 		System.out.println("How often shall the program update time(in seconds) n"); 
+
+		boolean succes=false;
 		String input;
-		try { 
-			input=bufferRead.readLine(); 
+		while(!succes){
+			try { 
+				input=bufferRead.readLine(); 
 
-			try{ 
-				int newTime =Integer.parseInt(input); 
-				setFrequency(newTime); 
-			} catch(IllegalArgumentException e){ 
-				e.printStackTrace(); 
-			} 
+				try{ 
+					int newTime =Integer.parseInt(input); 
+					setFrequency(newTime); 
+					succes=true;
+				} catch(IllegalArgumentException e){ 
+					//e.printStackTrace(); 
+				} 
 
-		} catch (IOException e1) {  
-			e1.printStackTrace(); 
-		} 
+			} catch (IOException e1) {  
+				//e1.printStackTrace(); 
+			} finally{
+				input="";
+				if(!succes){
+					System.out.println("Choose a integre number to continue...");
+				}
+			}
+		}
+	}
 
+	private static void run(){
 		CommandClock cc = new CommandClock(cl); 
 
 		int i; 
@@ -51,19 +77,5 @@ public class App
 				e.printStackTrace();
 			}
 		} 
-		System.out.println("The claim that it is a fun date today are " + cc.funDate() );
-		LocalDateTime tp = LocalDateTime.now().withDayOfMonth(12);
-		Clock newCl = Clock.fixed(tp.atZone(ZoneId.systemDefault()).toInstant(), ZoneId.systemDefault());
-		cc.printClock(newCl);
-		
-		
-	}
-
-	private static void setFrequency(int newTime) {
-		frequency=newTime*1000;		
-	} 
-	
-	private static void setClock(Clock clock){
-		cl=clock;
 	}
 }
